164ef0a30ffb70c309daa8ef18f18fce9cb5a849
Vedant Deshpande <vedant.milind.deshpande@gmail.com>
2021-08-29 16:41:26 +0000

    To do v1

---
+++ src/components/Content.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import TaskCard from "./TaskCard";
+import TextField from "@material-ui/core/TextField";
+import SaveIcon from "@material-ui/icons/Save";
+import Button from "@material-ui/core/Button";
+
+export default function Content() {
+  const [tasks, setTasks] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+
+  function addTask(title, desc, imgurl) {
+    
+
+    setTasks([...tasks,  { title: title, desc: desc, imgurl: imgurl }]);
+    
+    setShowForm(false);
+  }
+
+  return (
+    <>
+      <div style={{ height: "85px" }}>
+        <AddCircleIcon
+          onClick={() => {
+            setShowForm(!showForm);
+          }}
+          style={{
+            width: "50px",
+            height: "50px",
+            color: "#3f51b5",
+            float: "right",
+            padding: "0.6em 1.2em",
+
+            transform: showForm ? "rotate(45deg)" : "rotate(0deg)",
+          }}
+        ></AddCircleIcon>
+      </div>
+      {showForm && (
+        <div style={{ margin: "2em" }}>
+          <div>
+            <TextField
+              helperText="Enter Task Title"
+              variant="outlined"
+              type="text"
+              name="tasktitle"
+              onChange={(data) => {
+                console.log(data)
+                setTitle(data.target.value);
+              }}
+            ></TextField>
+          </div>
+          <div>
+            <TextField
+              helperText="Enter Task Description"
+              variant="outlined"
+              type="text"
+              name="taskdesc"
+              onChange={(data) => {
+                setDesc(data.target.value);
+              }}
+            ></TextField>
+          </div>
+          <div>
+            <TextField
+              helperText="Enter Image URL"
+              variant="outlined"
+              type="text"
+              name="imgurl"
+              onChange={(data) => {
+                setImgUrl(data.target.value);
+              }}
+            ></TextField>
+          </div>
+          <Button
+            name="addtask"
+            variant="contained"
+            color="primary"
+            size="large"
+           style={{marginTop:"2em"}}
+            startIcon={<SaveIcon />}
+            onClick={() => {
+              addTask(title, desc, imgUrl);
+            }}
+          >
+           Save
+          </Button>
+        </div>
+      )}
+      <div style={{ padding: "3em" }}>
+        <TaskCard tasks={tasks}></TaskCard>
+      </div>
+    </>
+  );
+}
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Swal from 'sweetalert2'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function TaskCard({tasks}) {
+  
+
+  const classes = useStyles();
+
+  return (
+    <>
+      {tasks.map((res, index) => (
+        <span style={{padding:"5em"}}>
+        <Card key={index} className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={res.imgurl}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {res.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {res.desc}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <DeleteIcon style={{ color: "red" }}></DeleteIcon>
+          </CardActions>
+        </Card>
+        </span>
+      ))}
+    </>
+  );
+}

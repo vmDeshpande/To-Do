3002651b3ad61bf2dc2f5079e311f89a3e148cd1
Vedant Deshpande <vedant.milind.deshpande@gmail.com>
2021-08-29 17:09:36 +0000

    Delete Task

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -90,7 +90,7 @@ export default function Content() {
         </div>
       )}
       <div style={{ padding: "3em" }}>
-        <TaskCard tasks={tasks}></TaskCard>
+        <TaskCard tasks={tasks} setTasks={setTasks}></TaskCard>
       </div>
     </>
   );
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -8,7 +8,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import DeleteIcon from "@material-ui/icons/Delete";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const useStyles = makeStyles({
   root: {
@@ -19,35 +19,41 @@ const useStyles = makeStyles({
   },
 });
 
-export default function TaskCard({tasks}) {
-  
-
+export default function TaskCard({ tasks, setTasks }) {
   const classes = useStyles();
 
   return (
     <>
       {tasks.map((res, index) => (
-        <span style={{padding:"5em"}}>
-        <Card key={index} className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={res.imgurl}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {res.title}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {res.desc}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <DeleteIcon style={{ color: "red" }}></DeleteIcon>
-          </CardActions>
-        </Card>
+        <span style={{ padding: "5em" }} key={index}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={res.imgurl}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {res.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {res.desc}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <DeleteIcon
+                style={{ color: "red" }}
+                onClick={() => {
+                  let t = tasks;
+                  t = t.splice(index, 1);
+                  console.log(t.splice(index, 1));
+                  setTasks(t.splice(index, 1));
+                }}
+              ></DeleteIcon>
+            </CardActions>
+          </Card>
         </span>
       ))}
     </>
